6dc0217842584687f622ab20f134280115dcab3e
Takezo <Takezo@192.168.1.2>
2012-09-05 21:43:59 -0300

    Identificado os ManagedBeans para Funcionario, Perfil e Usuario.

--- src/saw/web/UsuarioBean.java
+++ src/saw/web/UsuarioBean.java
@@ -1,5 +1,11 @@
 package saw.web;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="usuarioBean")
+@RequestScoped
+
 public class UsuarioBean {
 
 }
