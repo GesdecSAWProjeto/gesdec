b3f2cbb4c2a90f506600fc1db57c1c37d51a466c
Takezo <Takezo@192.168.1.2>
2012-09-05 22:28:04 -0300

    Várias alterações.

--- src/saw/web/FuncionarioBean.java
+++ src/saw/web/FuncionarioBean.java
@@ -1,5 +1,11 @@
 package saw.web;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="funcionarioBean")
+@RequestScoped
+
 public class FuncionarioBean {
 
 }
